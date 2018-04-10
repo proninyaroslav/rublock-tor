7f3eb5a04dce21b90fd1b9b1fe9aa5f8e4480bf5
blackcofee <42971175+blackcofee@users.noreply.github.com>
2018-04-10 16:18:16 +0900

    Update rublupdate.lua

--- opt/usr/bin/rublupdate.lua
+++ opt/usr/bin/rublupdate.lua
@@ -13,7 +13,7 @@ local config = {
     ipsetConfigPath = "/opt/etc/runblock/runblock.ipset",
     ipsetDns = "rublack-dns",
     ipsetIp = "rublack-ip",
-    torDnsAddr = "1.1.1.1"
+    torDnsAddr = "8.8.8.8"
 }
  
  
