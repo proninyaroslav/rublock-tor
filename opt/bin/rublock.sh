719978a84448876c4ab3bc61829e3b1fef6351b7
blackcofee <42971175+blackcofee@users.noreply.github.com>
2018-07-24 14:06:13 +0000

    rublock.sh

---
+++ opt/bin/rublock.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+echo List Generation
+/opt/bin/blupdate.lua
+
+echo Clear List
+cd /opt/etc/runblock
+sed -i '/pornhub.com/d' runblock.dnsmasq
+sed -i '/youtube.com/d' runblock.dnsmasq
+sed -i '/googleusercontent.com/d' runblock.dnsmasq
+sed -i '$aipset=\/nnm-club.ws\/rublock' runblock.dnsmasq
+sed -i '$aipset=\/gnome-look.org\/rublock' runblock.dnsmasq
+sed -i '$aipset=\/opendesktop.org\/rublock' runblock.dnsmasq
+sed -i '$aipset=\/pling.com\/rublock' runblock.dnsmasq
+sed -i '$a52.77.181.198' runblock.ipset
+sed -i '$a54.229.110.205' runblock.ipset
+
+echo Restart dnsmasq
+killall -sighup dnsmasq
