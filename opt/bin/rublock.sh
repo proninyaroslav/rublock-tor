94efa1c66b103931e75c12113e911a27ee8133e8
blackcofee <42971175+blackcofee@users.noreply.github.com>
2018-08-31 13:43:48 +0000

    runblock.ipset

--- opt/bin/rublock.sh
+++ opt/bin/rublock.sh
@@ -15,15 +15,15 @@ sed -i '$aipset=\/pling.com\/rublock' runblock.dnsmasq
 sed -i '$a52.77.181.198' runblock.ipset
 sed -i '$a54.229.110.205' runblock.ipset
 sed -i '$a18.205.93.0\/25' runblock.ipset
-sed -i '$a91.108.4.0\/22' rublock.ipset
-sed -i '$a91.108.8.0\/22' rublock.ipset
-sed -i '$a91.108.12.0\/22' rublock.ipset
-sed -i '$a91.108.16.0\/22' rublock.ipset
-sed -i '$a91.108.56.0\/22' rublock.ipset
-sed -i '$a149.154.160.0\/22' rublock.ipset
-sed -i '$a149.154.164.0\/22' rublock.ipset
-sed -i '$a149.154.168.0\/22' rublock.ipset
-sed -i '$a149.154.172.0\/22' rublock.ipset
+sed -i '$a91.108.4.0\/22' runblock.ipset
+sed -i '$a91.108.8.0\/22' runblock.ipset
+sed -i '$a91.108.12.0\/22' runblock.ipset
+sed -i '$a91.108.16.0\/22' runblock.ipset
+sed -i '$a91.108.56.0\/22' runblock.ipset
+sed -i '$a149.154.160.0\/22' runblock.ipset
+sed -i '$a149.154.164.0\/22' runblock.ipset
+sed -i '$a149.154.168.0\/22' runblock.ipset
+sed -i '$a149.154.172.0\/22' runblock.ipset
 
 echo Restart dnsmasq
 restart_dhcpd
