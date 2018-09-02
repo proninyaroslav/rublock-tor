7c3f8a8432f0298131f08bad7d4b8dfede7622e8
blackcofee <42971175+blackcofee@users.noreply.github.com>
2018-09-02 11:38:33 +0000

    rublupdate.lua

--- opt/bin/rublupdate.lua
+++ opt/bin/rublupdate.lua
@@ -7,7 +7,7 @@ local config = {
     neverGroupDomains = { ["livejournal.com"] = true, ["facebook.com"] = true , ["vk.com"] = true },
     stripWww = true,
     convertIdn = false,
-    torifyNsLookups = true, -- отправлять DNS запросы заблокированных доменов через TOR
+    torifyNsLookups = false, -- отправлять DNS запросы заблокированных доменов через TOR
     blMinimumEntries = 1000, -- костыль если список получился короче, значит что-то пошло не так и конфиги не обновляем
     dnsmasqConfigPath = "/opt/etc/runblock/runblock.dnsmasq",
     ipsetConfigPath = "/opt/etc/runblock/runblock.ipset",
