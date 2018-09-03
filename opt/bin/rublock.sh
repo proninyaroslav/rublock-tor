be743e69d9c31863bd7a356bceb7fb214f6f442d
blackcofee <42971175+blackcofee@users.noreply.github.com>
2018-09-03 12:26:33 +0000

    rublock.sh update

--- opt/bin/rublock.sh
+++ opt/bin/rublock.sh
@@ -8,10 +8,10 @@ cd /opt/etc/runblock
 sed -i '/pornhub.com/d' runblock.dnsmasq
 sed -i '/youtube.com/d' runblock.dnsmasq
 sed -i '/googleusercontent.com/d' runblock.dnsmasq
-sed -i '$aipset=\/nnm-club.ws\/rublock' runblock.dnsmasq
-sed -i '$aipset=\/gnome-look.org\/rublock' runblock.dnsmasq
-sed -i '$aipset=\/opendesktop.org\/rublock' runblock.dnsmasq
-sed -i '$aipset=\/pling.com\/rublock' runblock.dnsmasq
+sed -i '$aipset=\/nnm-club.ws\/rublack-dns' runblock.dnsmasq
+sed -i '$aipset=\/gnome-look.org\/rublack-dns' runblock.dnsmasq
+sed -i '$aipset=\/opendesktop.org\/rublack-dns' runblock.dnsmasq
+sed -i '$aipset=\/pling.com\/rublack-dns' runblock.dnsmasq
 sed -i '$a52.77.181.198' runblock.ipset
 sed -i '$a54.229.110.205' runblock.ipset
 sed -i '$a18.205.93.0\/25' runblock.ipset
