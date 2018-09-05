9054e76210c62268eceb0b2c7af42089ba9633ee
blackcofee <42971175+blackcofee@users.noreply.github.com>
2018-09-05 11:52:08 +0900

    Create uablock.sh

---
+++ opt/bin/uablock.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo Download Lists 
+wget -O /opt/etc/runblock/runblock.dnsmasq https://raw.githubusercontent.com/blackcofee/uablock-list/master/urlblock
+wget -O /opt/etc/runblock/runblock.ipset https://raw.githubusercontent.com/blackcofee/uablock-list/master/ipblock
+
+echo Generation Block List
+cd /opt/etc/runblock
+sed -i 's/.*/ipset=\/&\/rublack-dns/' runblock.dnsmasq
+
+echo Restart dnsmasq
+restart_dhcpd
+restart_firewall
