c7168882ce5e4434a69395ad168950a4795507c9
blackcofee <42971175+blackcofee@users.noreply.github.com>
2019-01-09 18:42:57 +0900

    Update rublock.sh

--- opt/bin/rublock.sh
+++ opt/bin/rublock.sh
@@ -29,7 +29,7 @@ sed -i '$a149.154.164.0\/22' runblock.ipset
 sed -i '$a149.154.168.0\/22' runblock.ipset
 sed -i '$a149.154.172.0\/22' runblock.ipset
 
-echo Add ip's
+echo Add ip
 ipset flush rublack-dns
 
 for IP in $(cat /opt/etc/runblock/runblock.ipset) ; do
