b28f7a8c0826c34af248d9fe4d2878d8a0d250d2
blackcofee <42971175+blackcofee@users.noreply.github.com>
2019-02-14 11:32:37 +0900

    Update rublupdate.lua

--- opt/bin/rublupdate.lua
+++ opt/bin/rublupdate.lua
@@ -2,7 +2,7 @@
 
 local config = {
     blSource = "antizapret", -- antizapret или rublacklist
-    groupBySld = 32, -- количество поддоменов после которого в список вносится весь домен второго уровня целиком
+    groupBySld = 16, -- количество поддоменов после которого в список вносится весь домен второго уровня целиком
     neverGroupMasks = { "^%a%a%a?.%a%a$" }, -- не праспространять на org.ru, net.ua и аналогичные
     neverGroupDomains = { ["livejournal.com"] = true, ["facebook.com"] = true , ["vk.com"] = true },
     stripWww = true,
