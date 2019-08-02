373a0ae3e92baed09c0ed3ac3d7c90e47ac42e68
blackcofee <42971175+blackcofee@users.noreply.github.com>
2019-08-02 18:53:58 +0900

    Update rublock.sh

--- opt/bin/rublock.sh
+++ opt/bin/rublock.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
-echo List Generation
+echo List generation
 /opt/bin/rublupdate.lua
 
-echo Clear List
+echo Clear the list
 cd /opt/etc/runblock
 sed -i '/pornhub.com/d' runblock.dnsmasq
-sed -i '/youtube.com/d' runblock.dnsmasq
-sed -i '/googleusercontent.com/d' runblock.dnsmasq
+sed -i '/youtube.com/d; /googleusercontent.com/d' runblock.dnsmasq
+sed -i '/vulkan/d; /wulkan/d; /vulcan/d' runblock.dnsmasq
+sed -i '/casino/d; /kasino/d; /azino/d' runblock.dnsmasq
+sed -i '/1xbet/d; /betcity/d; /leon/d; /fonbet/d' runblock.dnsmasq
+sed -i '/stavka/d; /slot/d; /dosug/d' runblock.dnsmasq
+
+echo Add custom sites
 sed -i '$aipset=\/nnm-club.ws\/rublack-dns' runblock.dnsmasq
 sed -i '$aipset=\/gnome-look.org\/rublack-dns' runblock.dnsmasq
 sed -i '$aipset=\/opendesktop.org\/rublack-dns' runblock.dnsmasq
@@ -16,6 +21,8 @@ sed -i '$aipset=\/7-zip.org\/rublack-dns' runblock.dnsmasq
 sed -i '$aipset=\/reactos.org\/rublack-dns' runblock.dnsmasq
 sed -i '$aipset=\/nextcloud.com\/rublack-dns' runblock.dnsmasq
 sed -i '$aipset=\/git.openwrt.org\/rublack-dns' runblock.dnsmasq
+
+echo Add custom ip
 sed -i '$a91.108.4.0\/22' runblock.ipset
 sed -i '$a91.108.8.0\/22' runblock.ipset
 sed -i '$a91.108.12.0\/22' runblock.ipset
@@ -26,7 +33,7 @@ sed -i '$a149.154.164.0\/22' runblock.ipset
 sed -i '$a149.154.168.0\/22' runblock.ipset
 sed -i '$a149.154.172.0\/22' runblock.ipset
 
-echo Add ip
+echo Add ip to table
 ipset flush rublack-dns
 
 for IP in $(cat /opt/etc/runblock/runblock.ipset) ; do
