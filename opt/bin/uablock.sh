f3ff7e37d4ba535a8856951d7df5eb5a80df91ce
blackcofee <42971175+blackcofee@users.noreply.github.com>
2019-09-14 20:09:18 +0900

    Update uablock.sh

--- opt/bin/uablock.sh
+++ opt/bin/uablock.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 echo Download Lists 
-wget -O /opt/etc/rublock.dnsmasq https://raw.githubusercontent.com/blackcofee/uablock-list/master/urlblock
-wget -O /opt/etc/rublock.ips https://raw.githubusercontent.com/blackcofee/uablock-list/master/ipblock
+wget -O /opt/etc/rublock/rublock.dnsmasq https://raw.githubusercontent.com/blackcofee/uablock-list/master/urlblock
+wget -O /opt/etc/rublock/rublock.ips https://raw.githubusercontent.com/blackcofee/uablock-list/master/ipblock
 
 echo Generation Block List
-cd /opt/etc/
+cd /opt/etc/rublock
 sed -i 's/.*/ipset=\/&\/rublock/' rublock.dnsmasq
 
 echo Add ip
 ipset flush rublock
 
-for IP in $(cat /opt/etc/rublock.ips) ; do
+for IP in $(cat /opt/etc/rublock/rublock.ips) ; do
 ipset -A rublock $IP
 done
 
