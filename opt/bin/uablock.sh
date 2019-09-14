e3d303309f083fa022185fa62951fe78145b58bf
blackcofee <42971175+blackcofee@users.noreply.github.com>
2019-09-14 20:06:54 +0900

    Update uablock.sh

--- opt/bin/uablock.sh
+++ opt/bin/uablock.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 echo Download Lists 
-wget -O /opt/etc/runblock/runblock.dnsmasq https://raw.githubusercontent.com/blackcofee/uablock-list/master/urlblock
-wget -O /opt/etc/runblock/runblock.ipset https://raw.githubusercontent.com/blackcofee/uablock-list/master/ipblock
+wget -O /opt/etc/rublock.dnsmasq https://raw.githubusercontent.com/blackcofee/uablock-list/master/urlblock
+wget -O /opt/etc/rublock.ips https://raw.githubusercontent.com/blackcofee/uablock-list/master/ipblock
 
 echo Generation Block List
-cd /opt/etc/runblock
-sed -i 's/.*/ipset=\/&\/rublack-dns/' runblock.dnsmasq
+cd /opt/etc/
+sed -i 's/.*/ipset=\/&\/rublock/' rublock.dnsmasq
 
 echo Add ip
-ipset flush rublack-dns
+ipset flush rublock
 
-for IP in $(cat /opt/etc/runblock/runblock.ipset) ; do
-ipset -A rublack-dns $IP
+for IP in $(cat /opt/etc/rublock.ips) ; do
+ipset -A rublock $IP
 done
 
 echo Restart dnsmasq
